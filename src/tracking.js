358b24038d6689be39abe8e450a5d7bcca835b87
Jonas Souza <jonasmzsouza@gmail.com>
2023-08-23 17:53:57 -0300

    :bug: fix: improve href construction logic

--- src/tracking.js
+++ src/tracking.js
@@ -1,30 +1,29 @@
 /**
-  * Evento de click disparado em todos os links do site para que os 
-  * parametros de url que vem de links patrocinados/orgânicos 
-  * sejam encaminhados até a formulário de inscrição.
-  * 
-  * Esta alternativa contempla links gerados em 
-  * campos customizados do wordpress.
-  * A regra só se aplica a links que contenham a origem (window.location.origin) 
-  * do site (https://www.domain.com) ou ficha de incrição (https://sub.domain.com) 
-  * Autor: Jonas Souza - 17/08/2023
-  */
-$("a").click(function(event) {
+ * Evento de click disparado em todos os links do site para que os
+ * parametros de url que vem de links patrocinados/orgânicos
+ * sejam encaminhados até a formulário de inscrição.
+ *
+ * Esta alternativa contempla links gerados em
+ * campos customizados do wordpress.
+ * A regra só se aplica a links que contenham a origem (window.location.origin)
+ * do site (https://www.domain.com) ou ficha de incrição (https://sub.domain.com)
+ * Autor: Jonas Souza - 17/08/2023
+ */
+$("a").click(function (event) {
   if (
-    !$(this).hasClass("linkassiste filter-button") && //se não for o elemento 'play' de vídeo
-    this.href.indexOf("mailto:") == -1 && //se não for email
-    this.href.indexOf("tel:") == -1 && //se não for telefone
-    $(this).attr("role") != "button" && //se nao for o elemento de controle carrossel
-    $(this).attr("data-toggle") != "dropdown" && //se não o elemento dropdown
-    $(this).attr("data-toggle") != "tab" && //se não o elemento tab
-    $(this).attr("data-toggle") != "modal"
+    !$(this).hasClass("linkassiste filter-button") && //se não for o elemento 'play' de vídeo ou btn filtro
+    this.href.indexOf("mailto:") == -1 && //e não for email
+    this.href.indexOf("tel:") == -1 && //e não for telefone
+    $(this).attr("role") != "button" && //e nao for o elemento de controle carrossel
+    $(this).attr("data-toggle") != "dropdown" && //e não for o elemento dropdown
+    $(this).attr("data-toggle") != "tab" && //e não for o elemento tab
+    $(this).attr("data-toggle") != "modal" //e não for o elemento que chama o modal
   ) {
     // se o link tiver a origem do site ou ficha de incrição
     if (
       origin == "https://www.domain.com" ||
       origin == "https://sub.domain.com"
     ) {
-      //se não o elemento que chama o modal
       let target = $(this).attr("target");
       let origin = this.origin;
       let hash = this.hash;
@@ -39,40 +38,53 @@ $("a").click(function(event) {
 
       let href;
 
-      if (locationSearch) { //se tem parametros no locationSearch
+      //se tem parametros no locationSearch
+      if (locationSearch) {
         href = origin + pathname + hash + locationSearch;
-      } else { //parametros no locationHash
-        if (this.href.indexOf("#") != -1) { //se href tem #
-          href = origin + pathname + locationHash; //locationHash com parametros
-        } else { //href sem #
-          let parseUrl = locationHash.split("?"); //retirar os parametros do locationHash
-          href = origin + pathname + "?" + parseUrl[1];
+        //então parametros no locationHash
+      } else {
+        //se href tem #
+        if (this.href.indexOf("#") != -1) {
+          //locationHash com parametros
+          href = origin + pathname + locationHash;
+          //então href sem #
+        } else {
+          //retirar os parametros do locationHash
+          let parseUrl = locationHash.split("?");
+          //se tem parametros
+          if (parseUrl[1]) {
+            href = origin + pathname + "?" + parseUrl[1];
+            //então href sem parametros
+          } else {
+            href = origin + pathname;
+          }
         }
       }
 
-      if (this.href.indexOf("#") != -1) { //se href tem #
+      //se href tem #
+      if (this.href.indexOf("#") != -1) {
         event.preventDefault(event);
-        a
-        if (locationPage == nextPage) { //mesma págin
+        //mesma página
+        if (locationPage == nextPage) {
           if (hash) {
             $("html, body").animate(
               {
-                scrollTop: $(hash).position().top
+                scrollTop: $(hash).position().top,
               },
               1000,
               "easeInOutExpo"
             );
           }
-          
-        } else { //pagina diferente
+          //pagina diferente
+        } else {
           if (target === "_blank") {
             window.open(href, "_blank");
           } else {
             window.location.href = href;
           }
         }
-       
-      } else {  //href sem #
+        //href sem #
+      } else {
         event.preventDefault(event);
         if (target === "_blank") {
           window.open(href, "_blank");
