453daf453d13e1117915c2a455e93fc0291bc76b
Jonas Souza <jonasmzsouza@gmail.com>
2024-01-31 17:07:03 -0300

    :sparkles: feat: handle specific forms and improve functions descriptions

--- src/tracking.js
+++ src/tracking.js
@@ -1,21 +1,31 @@
 /**
  * Autor: Jonas Souza
  * Data de Criação: 17/08/2023
- * Última atualização: 13/12/2023
+ * Última atualização: 31/01/2024
  *
- * Implementação para manipulação de links do site
+ * Implementação para manipulação de links do site e formulários de consulta
  * Está implementação mantém alguns paramêtros de pesquisa
  * que são de origem de anúncios para fins de traqueamento
  * e remoção de outros paramêtros de pesquisa quem são de
- * origem de páginas de pesquisa do site 'paramsSiteArray'.
+ * origem de páginas de pesquisa do site 'siteParamsArray'.
  */
 
 $(document).on("ready", function () {
+  /////////////////////////////////LINK/////////////////////////////////////
+  /**
+   * Adicionar um ouvinte de evento de clique apropriado aos links
+   */
+  $("a").each(function () {
+    if (shouldHandleLink(this)) {
+      $(this).click(handleLinkClick);
+    }
+  });
+
   /**
    * Função para verificar se um link deve ser manipulado
    * Útil para ignorar alguns tipos de link.
-   * @param linkElement HTMLElement
-   * @return bool
+   * @param {HTMLElement} linkElement
+   * @return {bool}
    */
   function shouldHandleLink(linkElement) {
     const ignoreClasses = ["linkassiste", "filter-button", "page-numbers"];
@@ -37,15 +47,85 @@ $(document).on("ready", function () {
   }
 
   /**
+   * Função para manipular o clique em links.
+   * Útil para verificar o link do elemento é para a origen do site,
+   * chamar funções com responsabilidades específicas e redirecionar o link
+   * @param {Event} event
+   */
+  function handleLinkClick(event) {
+    const linkElement = this;
+    const origin = linkElement.origin;
+    const pathname = linkElement.pathname;
+    const target = $(linkElement).attr("target");
+    const hash = linkElement.hash;
+    const page = origin + pathname;
+
+    const acceptOrigins = ["www.domain.com", "sub.domain.com"];
+    const ignorePathnames = ["/wp-admin/"];
+
+    // Verifica se o link deve ser manipulado
+    const shouldHandleLink =
+      acceptOrigins.some((acceptedOrigin) => origin.includes(acceptedOrigin)) &&
+      !ignorePathnames.some((ignoredPathname) =>
+        pathname.includes(ignoredPathname)
+      );
+
+    if (shouldHandleLink) {
+      const { href, isHashSymbolPresent } = generateHref(
+        linkElement,
+        origin,
+        pathname,
+        hash
+      );
+
+      handleLinkRedirect(event, isHashSymbolPresent, href, page, hash, target);
+    }
+  }
+
+  /**
+   * Gera o href considerando diferentes cenários
+   * @param {Array} linkElement
+   * @param {String} origin
+   * @param {String} pathname
+   * @param {String} hash
+   * @return {String} href
+   */
+  function generateHref(linkElement, origin, pathname, hash) {
+    const locationHash = window.location.hash;
+    const locationSearch = window.location.search;
+
+    let href = origin + pathname;
+    const isHashSymbolPresent = linkElement.href.indexOf("#") !== -1;
+
+    if (locationSearch) {
+      const newSearch = removeURLParams(locationSearch);
+      href += hash + newSearch;
+    } else if (isHashSymbolPresent) {
+      const parseUrl = locationHash.split("?");
+      const parseUrlHash = parseUrl[0];
+      const newSearch = removeURLParams(parseUrl[1]);
+      href += parseUrlHash + newSearch;
+    } else {
+      const parseUrl = locationHash.split("?");
+      if (parseUrl[1]) {
+        const newSearch = removeURLParams(parseUrl[1]);
+        href += newSearch;
+      }
+    }
+
+    return { href, isHashSymbolPresent };
+  }
+
+  /**
    * Função para remover paramêtros de busca da URL.
    * Útil navegação pós pesquisa no site
-   * @param paramsSiteArray Array
-   * @param search String
-   * @return urlParams String
+   * @param {String} search
+   * @return {String} urlParams
    */
-  function removeURLParams(paramsSiteArray, search) {
+  function removeURLParams(search) {
+    const siteParamsArray = ["s", "tipo", "categoria"];
     const urlParams = new URLSearchParams(search);
-    paramsSiteArray.forEach((param) => {
+    siteParamsArray.forEach((param) => {
       urlParams.delete(param);
     });
 
@@ -55,93 +135,110 @@ $(document).on("ready", function () {
   }
 
   /**
-   * Função para manipular o clique em links.
-   * Útil para verificar o link do elemento é para a origen do site,
-   * chamar funções com responsabilidades específicas e redirecionar o link
-   * @param event Event
+   * Redireciona o link para diferentes cenários
+   * @param {Event} event
+   * @param {bool} isHashSymbolPresent
+   * @param {String} href
+   * @param {String} page
+   * @param {String} hash
+   * @param {String} target
    */
-  function handleLinkClick(event) {
-    const linkElement = this;
-    const origin = linkElement.origin;
-    const pathname = linkElement.pathname;
-    const target = $(linkElement).attr("target");
-    const hash = linkElement.hash;
-    const nextPage = origin + pathname;
+  function handleLinkRedirect(
+    event,
+    isHashSymbolPresent,
+    href,
+    page,
+    hash,
+    target
+  ) {
+    event.preventDefault();
 
-    const acceptOrigins = [
-      "https://www.domain.com",
-      "https://sub.domain.com",
-      "www.domain.com",
-      "sub.domain.com",
-    ];
-    const ignorePathnames = ["/wp-admin/"];
-    const paramsSiteArray = ["s", "tipo", "categoria"];
+    const locationOrigin = window.location.origin;
+    const locationPathname = window.location.pathname;
+    const locationPage = locationOrigin + locationPathname;
 
-    if (origin.includes(acceptOrigins) && !pathname.includes(ignorePathnames)) {
-      const locationHash = window.location.hash;
-      const locationOrigin = window.location.origin;
-      const locationPathname = window.location.pathname;
-      const locationSearch = window.location.search;
-      const locationPage = locationOrigin + locationPathname;
-
-      let href;
-
-      // gera href abrangendo diversos cenários
-      if (locationSearch) {
-        const newSearch = removeURLParams(paramsSiteArray, locationSearch);
-        href = origin + pathname + hash + newSearch;
-      } else if (linkElement.href.indexOf("#") !== -1) {
-        const parseUrl = locationHash.split("?");
-        const parseUrlHash = parseUrl[0];
-        const newSearch = removeURLParams(paramsSiteArray, parseUrl[1]);
-        href = origin + pathname + parseUrlHash + newSearch;
-      } else {
-        const parseUrl = locationHash.split("?");
-        if (parseUrl[1]) {
-          const newSearch = removeURLParams(paramsSiteArray, parseUrl[1]);
-          href = origin + pathname + newSearch;
-        } else {
-          href = origin + pathname;
-        }
-      }
-
-      // redireciona o link para diversos cenários
-      if (linkElement.href.indexOf("#") !== -1) {
-        event.preventDefault();
-        if (locationPage === nextPage) {
-          if (hash) {
-            $("html, body").animate(
-              {
-                scrollTop: $(hash).position().top,
-              },
-              1000,
-              "easeInOutExpo"
-            );
-          }
-        } else {
-          if (target === "_blank") {
-            window.open(href, "_blank");
-          } else {
-            window.location.href = href;
-          }
+    if (isHashSymbolPresent) {
+      if (page === locationPage) {
+        if (hash) {
+          $("html, body").animate(
+            {
+              scrollTop: $(hash).position().top,
+            },
+            1000,
+            "easeInOutExpo"
+          );
         }
       } else {
-        event.preventDefault();
         if (target === "_blank") {
           window.open(href, "_blank");
         } else {
           window.location.href = href;
         }
       }
+    } else {
+      if (target === "_blank") {
+        window.open(href, "_blank");
+      } else {
+        window.location.href = href;
+      }
     }
   }
 
+  /////////////////////////////////FORMULARIO/////////////////////////////////////
+
   /**
-   * Adicionar um ouvinte de evento de clique apropriado aos links
+   * Adicionar um ouvinte de evento de submit apropriado aos formulários
    */
-  $("a").each(function () {
-    if (shouldHandleLink(this)) {
-      $(this).click(handleLinkClick);
+  $("form").submit(handleFormSubmit);
+
+  /**
+   * Função para verificar se um form deve ser manipulado
+   * Útil para aceitar alguns formulários de busca.
+   * @param {HTMLElement} formElement
+   * @return {bool} bool
+   */
+  function shouldHandleForm(formElement) {
+    const acceptIds = ["buscaPorTexto", "search-categoria"];
+    return (handleFormSubmit = acceptIds.some((acceptedId) =>
+      formElement.id.includes(acceptedId)
+    ));
+  }
+
+  /**
+   * Função para manipular o submit em formulários.
+   * Preserva URLSearchParams existente e adiciona as do formulário
+   * @param {Event} event
+   */
+  function handleFormSubmit(event) {
+    if (shouldHandleForm(this)) {
+      event.preventDefault();
+
+      const formElement = this;
+      const locationHash = window.location.hash;
+      let locationSearch = "";
+
+      // Verifica se há parâmetros de consulta na hash
+      if (locationHash.includes("?")) {
+        const hashParts = locationHash.split("?");
+        locationSearch = "?" + hashParts[1];
+      } else {
+        locationSearch = window.location.search;
+      }
+
+      if (locationSearch) {
+        const urlParams = new URLSearchParams(removeURLParams(locationSearch));
+
+        for (const [key, value] of urlParams) {
+          const input = document.createElement("input");
+          input.type = "hidden";
+          input.name = key;
+          input.value = value;
+          formElement.appendChild(input);
+        }
+      }
+
+      // Desanexa o manipulador de evento de envio de formulário após o primeiro envio
+      $(formElement).off("submit").submit();
     }
-  });
+  }
 });
